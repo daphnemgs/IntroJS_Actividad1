2c308c662e9d7804707734c1266d913a827b2a03
daphnemgs <daphnemgs@gmail.com>
2022-03-08 21:38:56 -0600

    Actividad1

---
+++ Actividad1.js
@@ -0,0 +1,25 @@
+
+// Ejercicio 1
+var numero = Number (prompt("Ingresa un número"))
+var operacion=numero * 2
+console.log(operacion)
+
+// Ejercicio 2
+var numero1 = Number (prompt("Ingresa un número 1"))
+var numero2 = Number (prompt("Ingresa un número 2"))
+var resta=numero1 - numero2
+console.log(resta)
+
+
+// Ejercicio 3
+var radio = Number (prompt("Ingresa el radio del círculo"))
+var area= (3.1416*(radio*radio))
+console.log(area)
+
+// Ejercicio 4
+var p = Number (prompt("Ingresa valor de p"))
+var D = Number (prompt("Ingresa valor de D"))
+var d = Number (prompt("Ingresa valor de p"))                       
+var area= (D*d)/2
+console.log(area)
+
